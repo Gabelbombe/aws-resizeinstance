fa48a32a7b95f0888a7812baae60651a6c7ddc80
ehime <dodomeki@gmail.com>
2016-01-27 07:15:17 -0800

    Created runner that will respond with instance id, private ip and dns record

--- cli-resize.sh
+++ cli-resize.sh
@@ -1,1 +1,18 @@
 #!/usr/local/bin/bash
+## cli-resize.sh engradepro demo app
+
+ENV='non'
+
+APPLICATION="${1}"  #engradepro
+ENVIRONMENT="${2}"  #qastg
+FUNCTION="${3}"     #app
+
+aws ec2 describe-instances                    \
+--region us-east-1                            \
+--profile $ENV                                \
+--filters                                     \
+  "Name=tag:Application,Values=$APPLICATION"  \
+  "Name=tag:Environment,Values=$ENVIRONMENT"  \
+  "Name=tag:Function,Values=$FUNCTION"        \
+--query 'Reservations[].Instances[].[PrivateIpAddress,InstanceId,Tags[?Key==`Name`].Value[]]' \
+--output text | sed '$!N;s/\n/ /'
